820ebaf2ed429537869056dd1b791a5fcbcd45d7
Cristian Torres <desancheztorres@gmail.com>
2020-09-05 09:07:14 +0100

    Let's begin with a test

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', 'ProjectController@index');
+
+Route::post('/projects', 'ProjectController@store');
