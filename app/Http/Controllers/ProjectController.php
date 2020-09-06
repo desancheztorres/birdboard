a0b9e15bdbe648c425b49060c46c5fea925274da
Cristian Torres <desancheztorres@gmail.com>
2020-09-06 21:15:35 +0100

    Scoping Projects

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,12 +7,17 @@ use App\Project;
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = Project::all();
+        $projects = auth()->user()->projects;
 
         return view('projects.index', compact('projects'));
     }
 
     public function show(Project $project) {
+
+        if(auth()->user()->isNot($project->owner)) {
+            abort(403);
+        }
+
         return view('projects.show', compact('project'));
     }
 
--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -14,5 +15,13 @@ class ProjectTest extends TestCase
         $project = factory('App\Project')->create();
 
         $this->assertEquals('/projects/' . $project->id, $project->path());
+    }
+
+    /** @test */
+    public function it_belongs_to_an_owner() {
+        $project = factory('App\Project')->create();
+
+        $this->assertInstanceOf(User::class, $project->owner);
+
     }
 }
