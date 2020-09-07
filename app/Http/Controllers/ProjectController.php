090ff204151e217f92c7cc91e7c2f122eb3c4736
Cristian Torres <desancheztorres@gmail.com>
2020-09-07 18:28:08 +0100

    The create project view

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -21,6 +21,10 @@ class ProjectController extends Controller
         return view('projects.show', compact('project'));
     }
 
+    public function create() {
+        return view('projects.create');
+    }
+
     public function store(Project $project) {
 
         auth()->user()->projects()->create(request()->validate([
