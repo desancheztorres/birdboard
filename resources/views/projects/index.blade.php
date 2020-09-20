45a2366f340eecfe4ae0130f5a07356bfc562366
Cristian Torres <desancheztorres@gmail.com>
2020-09-20 19:39:19 +0100

    Go Go Gadget Tailwind

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,17 +2,19 @@
 
 @section('content')
 
-<div class="flex items-center mb-3">
-    <a href="/projects/create">New Project</a>
-</div>
+    <div class="flex items-center mb-3">
+        <a href="/projects/create">New Project</a>
+    </div>
 
-<ul>
-    @forelse($projects as $project)
-        <li>
-            <a href="{{ $project->path() }}">{{ $project->title }}</a>
-        </li>
-    @empty
-        <li>No projects yet.</li>
-    @endforelse
-</ul>
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
+                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+                <div class="text-gray">{{ Str::limit($project->description, 100) }}</div>
+            </div>
+        @empty
+            <div>No projects yet.</div>
+        @endforelse
+    </div>
+
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+      boxShadow: {
+          default: '0 0 5px 0 rgba(0, 0, 0, 0.88)'
+      },
+    extend: {
+          colors: {
+              gray: {
+                  default: 'rgba(0, 0, 0, 0.4)',
+                  light: '#F5F6F9',
+              }
+          },
+    },
   },
   variants: {},
   plugins: [],
